4cee1051cbaad28c631c8a2c7b153d5a69c863a2
JoseLlontop <josellontop100@gmail.com>
2023-10-31 19:03:05 +0100

    Estructura del Back-End

---
+++ src/main/java/com/utn/TiendaOnlineBackEnd/TiendaOnlineBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.utn.TiendaOnlineBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlineBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaOnlineBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/TiendaOnlineBackEnd/TiendaOnlineBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.TiendaOnlineBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaOnlineBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
